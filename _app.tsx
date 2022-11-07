0c991a52292d379126c5583f489070b9cc1cbc53
M_Semenishch <m.semenishch@zakaz.global>
2022-11-07 17:09:08 +0200

    Remove all not needed

--- _app.tsx
+++
@@ -1,9 +0,0 @@
-import '../styles/globals.css';
-import { AppProps } from 'next/app';
-
-export default function App({ Component, pageProps }: AppProps) {
-      console.log('test w s w sw sa');
-
-
-  return <Component {...pageProps} />;
-}
