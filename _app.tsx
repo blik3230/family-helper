24ea384c57b25de7fa6f133b0f4e6af2b734f5f5
M_Semenishch <m.semenishch@zakaz.global>
2022-11-07 16:57:49 +0200

    Test commit

---
+++ _app.tsx
@@ -0,0 +1,9 @@
+import '../styles/globals.css'
+import { AppProps } from 'next/app';
+
+export default function App({ Component, pageProps }: AppProps) {
+      console.log('test w s w');
+
+
+  return <Component {...pageProps} />
+}
